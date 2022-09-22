ebc61c5de2e22e77502a0533a2cf02338fa77896
Dan Liljestig <39473290+liljestig@users.noreply.github.com>
2022-09-22 13:00:11 +0800

    See description
    Add move_ball.html
    Add move_ball.js
    Add move_ball2.html
    Add move_ball2.js
    Update shape.html
    Add shapes.html
    Add shapes2.html
    Update Singapore.html

---
+++ move_ball.js
@@ -0,0 +1,10 @@
+var velocity = 100;
+var position = 0;
+var ball = document.getElementById('ball');
+
+function moveBall() {
+    position = position + velocity;
+    ball.style.left = position + "px";
+}
+
+setInterval(moveBall, 100);
---
+++ move_ball2.js
@@ -0,0 +1,14 @@
+var velocity = 100;
+var position = 0;
+var Xmax = 500;
+var Xmin = 0;
+var reverse = false;
+var ball = document.getElementById('ball');
+
+function moveBall() {
+    if (!reverse) position = position + velocity; else position = position - velocity;
+    if (position <= Xmin || position >= Xmax) reverse = !reverse;
+    ball.style.left = position + "px";
+}
+
+setInterval(moveBall, 100);
