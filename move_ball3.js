42f6a47feee2eca5676c8291cf1a011ba83cd3a5
Dan Liljestig <39473290+liljestig@users.noreply.github.com>
2023-02-18 00:50:39 +0800

    Multiple files updates

---
+++ move_ball3.js
@@ -0,0 +1,24 @@
+var velocity = 100;
+var position = 0;
+var Xmin = 0;
+var Xmax = 500;
+var reverse = false;
+var positionY = 0;
+var Ymin = 0;
+var Ymax = 500;
+var reverseY = false;
+var ball = document.getElementById('ball');
+
+function moveBall() {
+
+    if (!reverse) position = position + velocity; else position = position - velocity;
+    if (position <= Xmin || position >= Xmax) reverse = !reverse;
+
+    if (!reverseY) positionY = positionY + velocity; else positionY = positionY - velocity;
+    if (positionY <= Ymin || positionY >= Ymax) reverseY = !reverseY;
+
+    ball.style.left = position + 'px';
+    ball.style.top = positionY + 'px';
+}
+
+setInterval(moveBall, 100);
